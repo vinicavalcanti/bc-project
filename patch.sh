48037bf885a98b1aa27cabe905a6d9279387b189
Vinicius Cavalcanti <vinicius.cavalcanti@rivendel.com.br>
2018-12-20 10:14:48 -0500

    Add patch script

--- patch.sh
+++ patch.sh
@@ -2,4 +2,4 @@
 ROLE="    - rolearn: $1\n      username: system:node:{{EC2PrivateDNSName}}\n      groups:\n    - system:bootstrappers\n    - system:nodes"
 kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
 cat /tmp/aws-auth-patch.yml
-#kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
+kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
